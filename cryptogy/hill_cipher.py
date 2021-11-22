255439462bef6658a2dbe8c20307858635298aa4
mpaulavizcaino <mvizcaino@unal.edu.co>
2021-11-21 19:11:16 -0500

    Up hill, permut and scream (opt) without analizer

--- cryptogy/hill_cipher.py
+++ cryptogy/hill_cipher.py
@@ -1,114 +1,92 @@
+from _typeshed import Self
 from cipher import Cipher, CryptAnalizer
 import numpy as np
+import math
+import random
+import egcd
 
 class HillCipher(Cipher):
-    def _init_(self, key = ""):
+    def _init_(self, key = "", alphabet = "abcdefghijklmnopqrstuvwxyz"):
+        super().__init__()
         self.key = self.iniKey(key)
+        self.alphabet = alphabet
 
     def generateRandomKey(self):
         return ""
     
     def validKey(self, key):
-        #
-        #
-        #
-        return super().validKey(key)
+        return True
 
-    def encode(self, cleartext: str):
-        m = cleartext.replace(" ", "")
-        C = makeKey()
-        len_check = len(m) % 2 == 0
-        if not len_check:
-            m += "0"
-        P = HillCipher.createMatrixIntToStr(m)
-        m_len = int(len(m) / 2)
-        encodeText = ""
-        for i in range(m_len):
-            row_0 = P[0][i] * C[0][0] + P[1][i] * C[0][1]
-            integer = int(row_0 % 26 + 65)
-            encodeText += chr(integer)
-            row_1 = P[0][i] * C[1][0] + P[1][i] * C[1][1]
-            integer = int(row_1 % 26 + 65)
-            encodeText += chr(integer)
+    def encode(self, cleartext: str, key):
+        cleartextNum = Cipher.textToInt(cleartext.lower())
+
+        splitCleartext = [
+            cleartextNum[i : i + int(key.shape[0])]
+            for i in range(0, len(cleartextNum), int(key.shape[0]))
+        ]
+
+        for splitCleartextNum in splitCleartext:
+            splitCleartextNum = np.transpose(np.asarray(splitCleartextNum))[:, np.newaxis]
+
+            while splitCleartextNum.shape[0] != key.shape[0]:
+                splitCleartextNum = np.append(splitCleartextNum, Cipher.textToInt([" "]))[:, np.newaxis]
+
+            numbers = np.dot(key, splitCleartextNum) % 26
+            n = numbers.shape[0]  # length ciphertext (numbers)
+
+            for idx in range(n):
+                number = int(numbers[idx, 0])
+            
+            encodeText = Cipher.intToText(number)
+
         return "".join(encodeText).upper()
 
-    def decode(self, ciphertext: str =""):
-        A = HillCipher.makeKey()
-        det = A[0][0] * A[1][1] - A[0][1] * A[1][0]
-        det = det % 26
-        multInv = HillCipher.findMultInv(det)
-        A_inv = A
-        A_inv[0][0], A_inv[1][1] = A_inv[1, 1], A_inv[0, 0]
-        A[0][1] *= -1
-        A[1][0] *= -1
-        for row in range(2):
-            for column in range(2):
-                A_inv[row][column] *= multInv
-                A_inv[row][column] = A_inv[row][column] % 26
+    def egcd(a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            g, y, x = egcd(b % a, a)
+            return (g, x - (b // a) * y, y)
 
-        P = HillCipher.createMatrixIntToStr(ciphertext)
-        msg_len = int(len(ciphertext) / 2)
+    def matrixModInv(matrix, modulus):
+
+        det = int(np.round(np.linalg.det(matrix)))
+        detInv = egcd(det, modulus)[1] % modulus 
+        matrixModulusInv = (
+            detInv * np.round(det * np.linalg.inv(matrix)).astype(int) % modulus
+        )
+
+        return matrixModulusInv
+
+    def decode(self, ciphertext: str, keyInv):
         decodedText = ""
-        for i in range(msg_len):
-            column_0 = P[0][i] * A_inv[0][0] + P[1][i] * A_inv[0][1]
-            n = int(column_0 % 26 + 65)
-            decodedText += chr(n)
-            column_1 = P[0][i] * A_inv[1][0] + P[1][i] * A_inv[1][1]
-            n = int(column_1 % 26 + 65)
-            decodedText += chr(n)
-        if decodedText[-1] == "0":
-            decodedText = decodedText[:-1]
+        ciphertextNum = Cipher.textToInt(ciphertext)
+
+        splitCiphertext = [
+            ciphertextNum[i : i + int(keyInv.shape[0])]
+            for i in range(0, len(ciphertextNum), int(keyInv.shape[0]))
+        ]
+
+        for splitCiphertextNum in splitCiphertext:
+            splitCiphertextNum = np.transpose(np.asarray(splitCiphertextNum))[:, np.newaxis]
+            numbers = np.dot(keyInv, splitCiphertextNum) % 26
+            n = numbers.shape[0]
+
+            for idx in range(n):
+                number = int(numbers[idx, 0])
+            decodedText += Cipher.intToText(number)
+
         return "".join(decodedText)
 
-    @staticmethod
-    def findMultInv(det):
-        multInv = -1
-        for i in range(26):
-            inverse = det * i
-            if inverse % 26 == 1:
-                multInv = i
-                break
-        return multInv
-
-    @staticmethod
-    def makeKey():
-        det = 0
-        A = None
-        while True:
-            cipher = input("Input 4 letter cipher: ")
-            A = HillCipher.createMatrixIntToStr(cipher)
-            det = A[0][0] * A[1][1] - A[0][1] * A[1][0]
-            det = det % 26
-            invElement = HillCipher.findMultInv(det)
-            if invElement == -1:
-                print("Determinant is not relatively prime to 26, uninvertible key")
-            elif np.amax(A) > 26 and np.amin(A) < 0:
-                print("Only a-z characters are accepted")
-                print(np.amax(A), np.amin(A))
-            else:
-                break
-        return A
-
-    @staticmethod
-    def createMatrixIntToStr(string):
-        ints = [HillCipher.chrToInt(c) for c in string]
-        leng = len(ints)
-        M = np.zeros((2, int(leng / 2)), dtype=np.int32)
-        i = 0
-        for column in range(int(leng / 2)):
-            for row in range(2):
-                M[row][column] = ints[i]
-                i += 1
-        return M
-
-    @staticmethod
-    def chrToInt(c):
-        c = c.upper()
-        n = ord(c) - 65
-        return n
+class HillCryptAnalizer(CryptAnalizer):
+    def __init__(self):
+        super().__init__()
 
 if __name__ == "_main_":
     cipher = HillCipher(key=1)
     cleartext = "helloworld"
+    ciphertext = ""
     encode = cipher.encode(cleartext)
     print(encode)
+    decode = cipher.decode(ciphertext)
+    print(decode)
---
+++ cryptogy/stream_ciphers.py
@@ -0,0 +1,189 @@
+from _typeshed import Self
+from cipher import Cipher, CryptAnalizer
+import numpy as np
+
+class StreamCipher(Cipher):
+    def __init__(self):
+        super().__init__()
+        self.key = self.iniKey(key)
+
+    def validKey(self, key):
+        try:
+            key = list(key)
+        except TypeError:
+            pass
+
+    def encode(cleartext, key):
+        encodeText = list(cleartext)
+        n = 0
+        while n < len(encodeText):
+            if encodeText[n] == "A" or encodeText[n] == "a":
+                encodeText[n] = 0
+            elif encodeText[n] == "B" or encodeText[n] == "b":
+                encodeText[n] = 1
+            elif encodeText[n] == "C" or encodeText[n] == "c":
+                encodeText[n] = 2
+            elif encodeText[n] == "D" or encodeText[n] == "d":
+                encodeText[n] = 3
+            elif encodeText[n] == "E" or encodeText[n] == "e":
+                encodeText[n] = 4
+            elif encodeText[n] == "F" or  encodeText[n] == "f":
+                encodeText[n] = 5
+            elif encodeText[n] == "G" or encodeText[n] == "g":
+                encodeText[n] = 6
+            elif encodeText[n] == "H" or encodeText[n] == "h":
+                encodeText[n] = 7
+            elif encodeText[n] == "I" or encodeText[n] == "i":
+                encodeText[n] = 8
+            elif encodeText[n] == "J" or encodeText[n] == "j":
+                encodeText[n] = 9
+            elif encodeText[n] == "K" or encodeText[n] == "k":
+                encodeText[n] = 10
+            elif encodeText[n] == "L" or encodeText[n] == "l":
+                encodeText[n] = 11
+            elif encodeText[n] == "M" or encodeText[n] == "m":
+                encodeText[n] = 12
+            elif encodeText[n] == "N" or encodeText[n] == "n":
+                encodeText[n] = 13
+            elif encodeText[n] == "O" or encodeText[n] == "o":
+                encodeText[n] = 14
+            elif encodeText[n] == "P" or encodeText[n] == "p":
+                encodeText[n] = 15
+            elif encodeText[n] == "Q" or encodeText[n] == "q":
+                encodeText[n] = 16
+            elif encodeText[n] == "R" or encodeText[n] == "r":
+                encodeText[n] = 17
+            elif encodeText[n] == "S" or encodeText[n] == "s":
+                encodeText[n] = 18
+            elif encodeText[n] == "T" or encodeText[n] == "t":
+                encodeText[n] = 19
+            elif encodeText[n] == "U" or encodeText[n] == "u":
+                encodeText[n] = 20
+            elif encodeText[n] == "V" or encodeText[n] == "v":
+                encodeText[n] = 21
+            elif encodeText[n] == "W" or encodeText[n] == "w":
+                encodeText[n] = 22
+            elif encodeText[n] == "X" or encodeText[n] == "x":
+                encodeText[n] = 23
+            elif encodeText[n] == "Y" or encodeText[n] == "y":
+                encodeText[n] = 24
+            elif encodeText[n] == "Z" or encodeText[n] == "z":
+                encodeText[n] = 25
+            elif encodeText[n] == "0":
+                encodeText[n] = 26
+            elif encodeText[n] == "1":
+                encodeText[n] = 27
+            elif encodeText[n] == "2":
+                encodeText[n] = 28
+            elif encodeText[n] == "3":
+                encodeText[n] = 29
+            elif encodeText[n] == "4":
+                encodeText[n] = 30
+            elif encodeText[n] == "5":
+                encodeText[n] = 31
+            elif encodeText[n] == "6":
+                encodeText[n] = 32
+            elif encodeText[n] == "7":
+                encodeText[n] = 33
+            elif encodeText[n] == "8":
+                encodeText[n] = 34
+            elif encodeText[n] == "9":
+                encodeText[n] = 35
+            else:
+                pass
+            n += 1
+
+        return "".join(encodeText).upper()
+   
+        
+    def decode(ciphertext, key):
+        decodeText = list(ciphertext)
+        i = 0
+        while i < len(decodeText):
+            decodeText[i] = decodeText[i] - key[i % len(key)]
+            while decodeText[i] < 0:
+                decodeText[i] += 36
+            if decodeText[i] > 35:
+                decodeText[i] = decodeText[i] % 36
+            i += 1
+
+        n = 0
+        while n < len(decodeText):
+            if decodeText[n] == 0:
+                decodeText[n] = "A"
+            elif decodeText[n] == 1:
+                decodeText[n] = "B"
+            elif decodeText[n] == 2:
+                decodeText[n] = "C"
+            elif decodeText[n] == 3:
+                decodeText[n] = "D"
+            elif decodeText[n] == 4:
+                decodeText[n] = "E"
+            elif decodeText[n] == 5:
+                decodeText[n] = "F"
+            elif decodeText[n] == 6:
+                decodeText[n] = "G"
+            elif decodeText[n] == 7:
+                decodeText[n] = "H"
+            elif decodeText[n] == 8:
+                decodeText[n] = "I"
+            elif decodeText[n] == 9:
+                decodeText[n] = "J"
+            elif decodeText[n] == 10:
+                decodeText[n] = "K"
+            elif decodeText[n] == 11:
+                decodeText[n] = "L"
+            elif decodeText[n] == 12:
+                decodeText[n] = "M"
+            elif decodeText[n] == 13:
+                decodeText[n] = "N"
+            elif decodeText[n] == 14:
+                decodeText[n] = "O"
+            elif decodeText[n] == 15:
+                decodeText[n] = "P"
+            elif decodeText[n] == 16:
+                decodeText[n] = "Q"
+            elif decodeText[n] == 17:
+                decodeText[n] = "R"
+            elif decodeText[n] == 18:
+                decodeText[n] = "S"
+            elif decodeText[n] == 19:
+                decodeText[n] = "T"
+            elif decodeText[n] == 20:
+                decodeText[n] = "U"
+            elif decodeText[n] == 21:
+                decodeText[n] = "V"
+            elif decodeText[n] == 22:
+                decodeText[n] = "W"
+            elif decodeText[n] == 23:
+                decodeText[n] = "X"
+            elif decodeText[n] == 24:
+                decodeText[n] = "Y"
+            elif decodeText[n] == 25:
+                decodeText[n] = "Z"
+            elif decodeText[n] == 26:
+                decodeText[n] = "0"
+            elif decodeText[n] == 27:
+                decodeText[n] = "1"
+            elif decodeText[n] == 28:
+                decodeText[n] = "2"
+            elif decodeText[n] == 29:
+                decodeText[n] = "3"
+            elif decodeText[n] == 30:
+                decodeText[n] = "4"
+            elif decodeText[n] == 31:
+                decodeText[n] = "5"
+            elif decodeText[n] == 32:
+                decodeText[n] = "6"
+            elif decodeText[n] == 33:
+                decodeText[n] = "7"
+            elif decodeText[n] == 34:
+                decodeText[n] = "8"
+            elif decodeText[n] == 35:
+                decodeText[n] = "9"
+            n += 1
+        return "".join(decodeText)
+
+class PermutationCryptAnalizer(CryptAnalizer):
+    def __init__(self):
+        super().__init__()
