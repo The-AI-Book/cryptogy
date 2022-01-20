79a2159ee39cbd2296e8d7a55ee3844be30a41aa
mpaulavizcaino <mvizcaino@unal.edu.co>
2022-01-20 03:27:14 -0500

    image for aes

--- cryptogy/aes.py
+++ cryptogy/aes.py
@@ -8,8 +8,10 @@ from typing import List
 from hashlib import pbkdf2_hmac
 from hmac import new as new_hmac, compare_digest
 from Crypto.Cipher import AES
-import cv2
-import numpy as np
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+from base64 import b64encode
 
 s_box = (
     0x63, 
@@ -1146,10 +1148,125 @@ def decrypt_image(key, iv, encryptionMode: str, image, filename: str):
     cv2.imwrite(filename, dec_img)
     return True
 
+###################################
+
+    url = None  # ruta 
+    image = None  # objeto de la imagen
+    image_name = None  # nombre sin extensiones
+    path = None  # ruta de la carpeta 
+    image_size = None  # resolucion 
+    image_ext = None
+
+    # Cipher
+    key = None
+    iv = None
+    mode = AES.MODE_ECB
+
+    def __init__(self):
+        pass
+
+    def setImagePath(self, ruta: str):
+        self.url = ruta
+        aux = ruta.split("/")
+        name = aux[-1].split(".")
+        self.image_ext = name[-1]
+        self.image_name = name[0]
+        self.path = self.url.replace(aux[-1], "")
+
+    def setKeyImage(self, key: bytes):
+        self.key = pad(key, 16)
+
+    def setIvImage(self, iv: bytes):
+        self.iv = pad(iv, 16)
+
+    def setModeImage(self, modo: str):
+        if(modo == 'ECB'):
+            self.mode = AES.MODE_ECB
+        elif(modo == 'CBC'):
+            self.mode = AES.MODE_CBC
+        elif(modo == 'CFB'):
+            self.mode = AES.MODE_CFB
+        elif(modo == 'OFB'):
+            self.mode = AES.MODE_OFB
+        else:
+            print("Mode not recognized")
+
+    def getModeImage(self):
+        if(self.mode == AES.MODE_ECB):
+            return "ECB"
+        elif(self.mode == AES.MODE_CBC):
+            return "CBC"
+        elif(self.mode == AES.MODE_CFB):
+            return "CFB"
+        elif(self.mode == AES.MODE_OFB):
+            return "OFB"
+        else:
+            return None
+
+    def encryptImage(self):
+        if(self.url != None and self.key != None):
+            print("Cifrando la imagen")
+            img = Image.open(self.url)
+            self.image = np.array(img)
+            # print(len(self.image))
+            self.image_size = img.size
+            #print(self.key, self.iv)
+            new_url = self.path + self.image_name + "_e" + self.getMode() + "." + self.image_ext
+
+            cipher = None
+            if(self.getMode() != "ECB"):
+                cipher = AES.new(self.key, self.mode, iv=self.iv)
+            else:
+                cipher = AES.new(self.key, self.mode)
+
+            ct_bytes = cipher.encrypt(
+                pad(
+                    self.image.tobytes(),
+                    AES.block_size,
+                )
+            )
+            img_data = np.frombuffer(ct_bytes)
+            # print(len(img_data))
+
+            image_nva = Image.frombuffer(
+                "RGB",
+                self.image_size,
+                img_data
+            )
+            image_nva.save(
+                new_url
+            )
+            print("Cifrado")
+
+    def decryptImage(self):
+        if(self.url != None and self.key != None):
+            print("Decifrando...")
+            img = Image.open(self.url)
+            self.image = np.array(img)
+            self.image_size = img.size
+
+            new_url = self.path + self.image_name + "_d" + self.getMode() + "." + self.image_ext
+            cipher = None
+            if(self.getMode() != "ECB"):
+                cipher = AES.new(self.key, self.mode, iv=self.iv)
+            else:
+                cipher = AES.new(self.key, self.mode)
+
+            aux = self.image.tobytes()
+            pt = cipher.decrypt(
+                aux
+            )
+
+            img_data = np.frombuffer(pt)
+
+            Image.frombuffer(
+                "RGB",
+                self.image_size,
+                img_data
+            ).save(
+                new_url
+            )
+            print("Decifrado")
+
 if __name__ == "__main__":
     cleartext = b"thisismycleartexttobeencryptedthreetimes"
-
-
-
-
-
